f062358f7983c1f53a60d068460e71ec5e587ad9
Stephen Cuppett <steve@cuppett.com>
2020-08-15 07:43:09 -0400

    Creating standalone repository for nginx builds

--- base/php-entrypoint.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-set -e
-
-/usr/bin/php /usr/local/src/smarty/process_templates.php
-
-exec "$@"
--- base/smarty/compile_templates.php
+++
@@ -1,4 +0,0 @@
-<?php
-include('/usr/local/src/smarty/smarty_setup.php');
-
-$smarty->compileAllTemplates();
